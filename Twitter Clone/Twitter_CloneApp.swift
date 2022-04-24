1701bd62d0710288e9965b086ae612196908b96c
Leonardo Lazzari <le.lazzari@reply.it>
2022-04-24 22:56:56 +0200

    Initial Commit

---
+++ Twitter Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter Clone
+//
+//  Created by CGMCONSULTING on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
